73eaabc87daa0ac68650178ce5e22f401454a945
Sireat <102405529+Sireat@users.noreply.github.com>
2022-09-15 14:54:03 -0700

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,19 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _isdigit - check for a digit
- * @c : character to check
- * Return:0 or 1
+ * _isdigit - checks if parameter is a digit between 0 and 9
+ * @c: input number
+ * Return: 1 if number between 0 to 9, otherwise 0
  */
-
 int _isdigit(int c)
 {
-	return (c >= 48 && c <= 57);
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 
 }
