536ea0d2e7659664083bf433b537f29529b1a3f5
Sireat <102405529+Sireat@users.noreply.github.com>
2022-09-15 14:55:38 -0700

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,12 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * mul - returns the product of its parameters
- * @a: int type number
- * @b: int type number
- * Return:0
+ * mul - multiplies two integers.
+ * @a: first number.
+ * @b: second number.
+ * Return: multiplication
  */
-
-
 int mul(int a, int b)
 {
-return (a * b);
+	return (a * b);
 }
