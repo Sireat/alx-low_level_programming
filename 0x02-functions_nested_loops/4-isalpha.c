e7ff68a956cac3bacffb08744bd45d5f2e613cc5
Sireat <102405529+Sireat@users.noreply.github.com>
2022-09-16 11:20:13 -0700

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,21 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _isalpha - check for lower case letter
- * @c : character to check
- * Return:0 or 1
+ * _isalpha - shows 1 if input isna letter. Otherwise 0
+ *
+ * @c: character in ASCII code
+ *
+ * Return: 1 for letters. 0 for rest
  */
-
 int _isalpha(int c)
 {
-	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
-
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
 }
