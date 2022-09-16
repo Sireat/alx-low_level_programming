11e5030bc8aaf87ce5fccc87f231a27a08e6adfa
Sireat <102405529+Sireat@users.noreply.github.com>
2022-09-16 11:32:45 -0700

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,25 +1,22 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdio.h>
+
 /**
- * print_to_98 - print to 98
- * @n : number to start from
- * Return:0 or 1
+ * print_to_98 - prints from any integer to 98
+ * @n: integer input
+ *
+ * Return: void
  */
 
 void print_to_98(int n)
 {
-
-while (n < 98)
-{
-	printf("%i, ", n);
-	n++;
-}
-while (n > 98)
-{
-	printf("%i, ", n);
-	n--;
-}
-
-printf("98");
-putchar('\n');
+	while (n != 98)
+	{
+		printf("%i, ", n);
+		if (n > 98)
+			n--;
+		else
+			n++;
+	}
+	printf("98\n");
 }
