73b868c6ca2bc079f31be53b3ffad83dd38003dc
Sireat <102405529+Sireat@users.noreply.github.com>
2022-09-16 11:26:34 -0700

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,49 +1,32 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
- * Return:void
+ * jack_bauer - prints the minutes of a day
+ *
+ * Return: no return
  */
-
 void jack_bauer(void)
 {
+	int a, b, c, d;
 
-int h1;
-int h2;
-int m1;
-int m2;
-int a = 9;
-
-h2 = 0;
-while (h2 <= 2)
-{
-	if (h2 == 2)
+	for (a = 48; a <= 50; a++)
 	{
-		/*Restrain to 23h, not 29*/
-		a = 3;
-	}
-	h1 = 0;
-	while (h1 <= a)
-	{
-		m2 = 0;
-		while (m2 <= 5)
+		for (b = 48; b <= 57; b++)
 		{
-			m1 = 0;
-			while (m1 <= 9)
+			for (c = 48; c <= 53; c++)
 			{
-				_putchar('0' + h2);
-				_putchar('0' + h1);
-				_putchar(':');
-				_putchar('0' + m2);
-				_putchar('0' + m1);
-				_putchar('\n');
-				m1++;
+				for (d = 48; d <= 57; d++)
+				{
+					if (a >= 50 && b >= 52)
+						break;
+					_putchar(a);
+					_putchar(b);
+					_putchar(58);
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+				}
 			}
-			m2++;
 		}
-		h1++;
 	}
-	h2++;
-}
-
 }
