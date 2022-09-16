0cbbb7a64fc99cb6e9b1c5bf7fb75d354114c1d9
Sireat <sireatag@gmail.com>
2022-09-15 21:41:19 -0700

    low level

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,45 @@
 #include <stdio.h>
-#include <unistd.h>
+
+
+
 /**
- * main - Entry point
- * Description: prints numbers
- * Return: Always 0 (success)
- */
+ *
+ *  * main - Prints numbers within 0 to 9, adding commas and spaces
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
+
 {
-	int i;
 
-for (i = '0'; i <= '9' ; i++)
-	{
-		putchar(i);
+		int i;
 
-	if (i != '9')
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
 
-	putchar('\n');
-	return (0);
+
+			for (i = 48; i < 58; i++)
+
+					{
+
+								putchar(i);
+
+										if (i != 57)
+
+													{
+
+																	putchar(',');
+
+																				putchar(' ');
+
+																						}
+
+											}
+
+				putchar('\n');
+
+					return (0);
+
 }
