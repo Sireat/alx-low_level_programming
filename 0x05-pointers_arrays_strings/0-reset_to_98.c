d42c4ced169327823b80a852c6361621eb3da4d2
Sireat <102405529+Sireat@users.noreply.github.com>
2022-09-19 12:28:46 -0700

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,9 @@
 #include "main.h"
-
 /**
- *
- *  * reset_to_98 - takes a pointer to a parameter and resets its value.
- *
- *   * @n: input integer
- *
- *    * Return: nil
- *
- *     */
+ * reset_to_98 - takes a pointer to a parameter and resets its value.
+ * @n: input integer
+ * Return: nil
+ */
 
 void reset_to_98(int *n)
 
