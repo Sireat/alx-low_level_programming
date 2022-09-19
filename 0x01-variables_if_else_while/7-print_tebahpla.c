15c9a0d00351176ece7931ed265de9c69282c2e0
Sireat <102405529+Sireat@users.noreply.github.com>
2022-09-19 13:56:52 -0700

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 /**
+ * main - Prints reversed alphabet
  *
- *  * main - Prints reversed alphabet
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 int i;
