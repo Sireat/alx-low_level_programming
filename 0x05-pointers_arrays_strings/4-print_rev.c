1382583299869329f968d2c102b544491ee210a4
Sireat <sireatag@gmail.com>
2022-09-19 10:40:11 -0700

    pointer array files

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_rev - to reverse a string
+ *
+ * @str: a string to reverse.
+ *
+ */
+void print_rev(char *s)
+{
+    s="I do not fear computers. I fear the lack of them - Isaac Asimov";
+
+        print_rev(s);
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+#include <unistd.h>
+
+
+
+/**
+ *
+ *  * _putchar - writes the character c
+ *
+ *   */
+
+int _putchar(char c)
+
+{
+
+		return (write(1, &c, 1));
+
+}
